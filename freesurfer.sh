e72daad2a4d726e046675d7999b449d781b640be
obVdo <97226920+obVdo@users.noreply.github.com>
2026-03-16 14:59:18 +0100

    Update freesurfer.sh

--- freesurfer.sh
+++ freesurfer.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 t1=$(jq -r '.t1' config.json)
-export FREESURFER_HOME=/usr/local/freesurfer
+export FREESURFER_HOME=${FREESURFER_HOME:-/opt/freesurfer}
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 export OMP_NUM_THREADS=8
 export SUBJECTS_DIR=$(pwd)
