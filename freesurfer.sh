f12c4eeaf3daefe3dd479ab69bdfcb595c9c369d
obVdo <97226920+obVdo@users.noreply.github.com>
2026-03-18 13:51:37 +0100

    Update freesurfer.sh

--- freesurfer.sh
+++ freesurfer.sh
@@ -10,8 +10,7 @@ export OMP_NUM_THREADS=8
 export SUBJECTS_DIR=$(pwd)
 
 rm -rf output freesurfer
-recon-all -i $t1 -subject output -all -parallel -openmp $OMP_NUM_THREADS
-
+recon-all -i $t1 -subject output -all -watershed -parallel -openmp $OMP_NUM_THREADS
 mkdir -p freesurfer
 mv output freesurfer
 
