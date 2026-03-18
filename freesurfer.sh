7fba616b44dae934793795878e87602c047eb906
obVdo <97226920+obVdo@users.noreply.github.com>
2026-03-18 12:20:35 +0100

    Update freesurfer.sh

--- freesurfer.sh
+++ freesurfer.sh
@@ -8,6 +8,9 @@ export FREESURFER_HOME=${FREESURFER_HOME:-/usr/local/freesurfer}
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 export OMP_NUM_THREADS=8
 export SUBJECTS_DIR=$(pwd)
+echo "FREESURFER_LICENSE value: $FREESURFER_LICENSE"
+echo "FS_LICENSE value: $FS_LICENSE"
+echo "license.txt content:"; cat license.txt
 
 rm -rf output freesurfer
 recon-all -i $t1 -subject output -all -parallel -openmp $OMP_NUM_THREADS
